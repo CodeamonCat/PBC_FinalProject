76f6edf2fd788ab613a006ec90fee30b9813f2f1
Codeamon <LC13E7.CF0cl@gmail.com>
2021-12-29 22:33:59 +0800

    Update the cover page and the game library

---
+++ CMKuanInNTU.py
@@ -0,0 +1,7 @@
+import CMKuan_library
+
+if __name__ == '__main__':
+    CMKGame = CMKuan_library.Game()
+    CMKGame.init_screen()
+    CMKGame.display_cover()
+    print("===End of display cover")
