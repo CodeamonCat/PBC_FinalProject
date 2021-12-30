d6c1492f9c83424fb35a2d78e6aaaad6a4495753
Codeamon <LC13E7.CF0cl@gmail.com>
2021-12-31 03:01:28 +0800

    deal the confick may occur wrong result Need recover

--- CMKuan_game.py
+++ CMKuan_game.py
@@ -17,13 +17,8 @@ class Game:
         self.__screen = None
         self.__screen_height = 600
         self.__screen_width = 800
-<<<<<<< HEAD
         self.__text_background_color = (255, 255, 255)   # white
         self.__text_color = (0, 0, 0)    # black
-=======
-        self.__text_background_color = (255, 255, 255)   # #32CD32_lightgreen
-        self.__text_color = (0, 0, 0)    # #00008B_darkblue
->>>>>>> de5a5b50a9519337c8293b4aa461e8933059ad62
         self.__text_size = 20
 
     def check_event(self):
@@ -93,33 +88,12 @@ class Game:
     def display_cover(self):
         cover_background = Game.get_image('image\start.jpg', self.__screen_width, self.__screen_height)
         self.__screen.blit(cover_background, (0, 0))
-<<<<<<< HEAD
         self.display_text_with_position("管爺每天過者0037睡覺，0939起床的日子", 28, (self.__screen_width//2), (self.__screen_height//2 - 200))
         self.display_text_with_position("卻被每天都在趕Deadline同學質疑他到底懂不懂台大校", 28, (self.__screen_width//2), (self.__screen_height//2 - 130))
         self.display_text_with_position("否則怎麼可能那麼爽QQ", 40, (self.__screen_width//2), (self.__screen_height//2 - 60))
         self.display_text_with_position("用方向鍵操控管爺，並且蒐集散落在台大各個角落的問題", 28, (self.__screen_width//2), (self.__screen_height//2 + 40))
         self.display_text_with_position("在時間內盡量獲得高分吧！", 28, (self.__screen_width//2), (self.__screen_height//2 + 110))
         self.display_text_with_position_color("~按任何按鍵開始~", 60, (self.__screen_width//2), (self.__screen_height//2 + 200), self.__text_background_color, (255, 0, 0))
-=======
-        text, text_rect = self.display_text("管爺每天過者0037睡覺，0939起床的日子", 28)
-        text_rect.center = self.__screen_width/2, (self.__screen_height/2) - 160
-        self.__screen.blit(text, text_rect)
-        text, text_rect = self.display_text("卻被每天都在趕Deadline同學質疑他到底懂不懂台大校園", 28)
-        text_rect.center = self.__screen_width/2, (self.__screen_height/2) - 130
-        self.__screen.blit(text, text_rect)
-        text, text_rect = self.display_text("否則怎麼可能那麼爽QQ", 28)
-        text_rect.center = self.__screen_width/2, (self.__screen_height/2) - 40
-        self.__screen.blit(text, text_rect)
-        text, text_rect = self.display_text("~按任何按鍵開始~", 38)
-        text_rect.center = self.__screen_width/2, (self.__screen_height/2) + 200
-        self.__screen.blit(text, text_rect)
-        text, text_rect = self.display_text("用方向鍵操控管爺，並且蒐集散落在台大各個角落的問題", 28)
-        text_rect.center = self.__screen_width/2, (self.__screen_height/2) + 30
-        self.__screen.blit(text, text_rect)
-        text, text_rect = self.display_text("在時間內盡量獲得高分吧！", 28)
-        text_rect.center = self.__screen_width/2, (self.__screen_height/2) + 100
-        self.__screen.blit(text, text_rect)
->>>>>>> de5a5b50a9519337c8293b4aa461e8933059ad62
         pygame.display.update()
 
         display_cover = True
